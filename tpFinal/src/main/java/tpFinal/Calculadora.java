0f2c911389b61c17282f1c629bb573a582e5229f
gastonverdoia@gmail.com <gastonverdoia@gmail.com>
2021-06-18 21:36:01 -0300

    primer commit

---
+++ tpFinal/src/main/java/tpFinal/Calculadora.java
@@ -0,0 +1,8 @@
+package tpFinal;
+
+public class Calculadora {
+
+	public Integer sumer(Integer a, Integer b) {
+		return a + b;
+	}
+}
---
+++ tpFinal/src/test/java/tpFinal/CalculadoraTest.java
@@ -0,0 +1,14 @@
+package tpFinal;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class CalculadoraTest {
+
+  @Test
+  public void sumerTest() {
+    Calculadora calculadora = new Calculadora();
+    Integer result = 5;
+    AssertJUnit.assertEquals(result, calculadora.sumer(3, 2));
+  }
+}
